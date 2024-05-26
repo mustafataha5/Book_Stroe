6bdff0991517166cc01171d74ec97cf3d9030869
mustafataha5 <mustafa.taha.mu95@gmail.com>
2024-05-26 13:17:57 +0300

    update DB 2

--- project_app/models.py
+++ project_app/models.py
@@ -107,8 +107,8 @@ class Language(models.Model):
     objects = LanguageManger()
     
 class Author(models.Model): 
-    first_name = models.CharField(max_length=45,default=' ')
-    last_name = models.CharField(max_length=45,default=' ')
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True) 
     
